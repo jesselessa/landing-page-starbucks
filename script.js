81bd7867d4c15541987d2f5035268f224b1de4e5
jesselessa <jessica.elessa@gmail.com>
2023-05-11 10:29:00 +0200

    added meta in index.html to prevent landscape orientation

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#burger");
+const nav = document.querySelector("#burger-menu");
+
+btn.addEventListener("click", navToggle);
+function navToggle() {
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no-scroll");
+}
