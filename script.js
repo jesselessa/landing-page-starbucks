e2e3ddcb2e3ea723de806f247b38a85ea0dcf9b3
jesselessa <jessica.elessa@gmail.com>
2023-05-12 06:11:15 +0200

    created function to change beverages images and circle bg color on click

--- script.js
+++ script.js
@@ -2,10 +2,27 @@
 const btn = document.querySelector("#burger");
 const nav = document.querySelector("#burger-menu");
 
-btn.addEventListener("click", navToggle);
-function navToggle() {
+btn.addEventListener("click", toggleNav);
+function toggleNav() {
   btn.classList.toggle("open");
   nav.classList.toggle("hidden");
-  document.body.classList.toggle("no-scroll");
 }
 
+// CHANGE BEVERAGES IMAGES AND BG COLOR ON CLICK
+const bigImg = document.querySelector("main .matcha");
+const thumbnails = Array.from(document.querySelectorAll(".thumbnails li img"));
+const circle = document.querySelector(".circle");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", changeImgBgColor);
+});
+function changeImgBgColor(e) {
+  bigImg.src = e.target.src;
+  if (e.target === thumbnails[0]) {
+    circle.style.backgroundColor = "#006241";
+  } else if (e.target === thumbnails[1]) {
+    circle.style.backgroundColor = "#eb7495";
+  } else {
+    circle.style.backgroundColor = "#d752b1";
+  }
+}
